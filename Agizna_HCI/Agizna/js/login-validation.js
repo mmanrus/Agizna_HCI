4de41f0516400ebad26263551f4de1fa4118b9b0
mmanrus <143089861+mmanrus@users.noreply.github.com>
2024-04-16 19:57:47 +0800

    Add files via upload

---
+++ Agizna_HCI/Agizna/js/login-validation.js
@@ -0,0 +1,36 @@
+function login() {
+    var enteredUsername = document.getElementById('login-user').value;
+    var enteredPassword = document.getElementById('login-password').value;
+    var flag = 0;
+
+    // Check if entered credentials match admin credentials
+    if (enteredUsername === 'admin' && enteredPassword === 'admin') {
+        window.location.href = "/Agizna/admin/pages/index.html";
+        alert('Admin Login Successful');
+        return;
+    }
+
+    // Your login authentication logic for registered users should go here
+    var storedUsers = JSON.parse(localStorage.getItem('users'));
+    if (storedUsers) {
+        for (var i = 0; i < storedUsers.length; i++) {
+            if ((enteredUsername === storedUsers[i].username || enteredUsername === storedUsers[i].email) && enteredPassword === storedUsers[i].password) {
+                alert('Login Successful');
+                flag = 1;
+                break;
+            }
+        }
+    }
+    if (flag === 0) {
+        alert(`Invalid ${enteredUsername} or Password`);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#login-btn').onclick = login;
+    document.getElementById('login-password').addEventListener('keyup', function(event) {
+        if (event.key === 'Enter') {
+            login();
+        }
+    })
+});
