901c49ab374ad627dabfa3880350ad8b97ee8baf
Nino Joseph Ruiz <joseph.ruiz1242@gmail.com>
2024-04-19 18:49:22 +0800

    This is our homepage

---
+++ Agizna_HCI/Agizna/javascript.js
@@ -0,0 +1,39 @@
+var swiper = new Swiper(".home-slider",{
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".room-slider", {
+    spaceBetween: 20,
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination:{
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+});
